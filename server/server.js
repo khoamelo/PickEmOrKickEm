2b90b90b7dd135e635ad14d19f7b5c14264add77
Khoamelo <ckn2004@MSI>
2025-05-26 13:19:31 -0700

    Imported new middleware (morgan) and new test routes (getting players and adding players). Added .env file and installed nodemon

--- server/server.js
+++ server/server.js
@@ -1,17 +1,39 @@
+require('dotenv').config()
 const express = require('express');
+const morgan = require('morgan');
 
+// Create an Express application
 const app = express();
 
-const port = 3000;
+app.use(express.json());
 
+//API routes
+app.get('/players', (req, res) => {
+    res.status(200).json({
+        player_id: 1234,
+        player_name: "LeBron James",
+        team_id: 3425,
+        position: "Forward",
+    });
+    console.log('Players route was hit');
+});
+
+app.post('/addPlayers', (req, res) => {
+    console.log("You have added a new player");
+    console.log(req.body);
+});
+
+// Define the port number the server will listen on
+const port = process.env.PORT || 3000;
+
+// Define a route handler for the root URL ("/")
+// When a GET request is made to "/", send a response back to the client
 app.get('/', (req, res) => {
-    res.send('Hello everybody from the server side! This is KMELO.')
+    res.send('Hello everybody from the server side! This is KMELO.');
 });
 
-app.get('/boolymon', (req, res) => {
-    res.send('This is for Boolymon, the greatest producer ever!')
-});
-
+// Start the server and make it listen on the defined port (3000)
+// Once the server is running, log a message to the console
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
