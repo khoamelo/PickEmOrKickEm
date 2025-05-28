2e987d6ee95aed0a6e305a4f236d5eafa3d1d65d
Khoamelo <ckn2004@MSI.localdomain>
2025-05-28 16:31:30 -0700

    Added .gitignore for environment files and node modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1>PICK 'EM OR KICK 'EM</h1>
-      <h2>Welcome to the Game!</h2>
+      <h1 className='text-center text-primary' style={{wordSpacing: '5px', letterSpacing: '2px'}}>
+        PICK 'EM OR KICK 'EM
+      </h1>
+      
+      <h2 className='text-center'>
+        Find the optimal bet!
+      </h2>
     </>
   )
 }
