ebbe7d4817d6e83ed9fd968765fb268cb17b50ed
Khoamelo <ckn2004@MSI.localdomain>
2025-06-02 18:45:08 -0700

    Added temp StatGraph page for visualizing the stats w/ graphs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import Login from './components/Login';
 import Home from './components/Home';
 import CheckPlayer from './components/CheckPlayer';
 import React from 'react'
+import Head2HeadGames from './components/Head2HeadGames';
+import LastNGames from './components/LastNGames';
+import StatGraph from './components/StatGraph';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path='/' element={<Login />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/check-player' element={<CheckPlayer />}></Route>
+        <Route path='/head-to-head' element={<Head2HeadGames />}></Route>
+        <Route path='/last-n-games' element={<LastNGames />}></Route>
+        <Route path='/results' element={<StatGraph />}></Route>
       </Routes>
     </Router>
   )
---
+++ client/src/components/StatGraph.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatGraph = () => {
+  return (
+    <div>
+        Bar chart will be rendered here
+    </div>
+  )
+}
+
+export default StatGraph
