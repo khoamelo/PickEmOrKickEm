92be890431729758f531d33bba4c9fde868b5dd6
Khoamelo <ckn2004@MSI.localdomain>
2025-06-10 16:00:52 -0700

    Added slider to check for minutes played threshold

--- client/src/components/StatGraph.jsx
+++ client/src/components/StatGraph.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import annotationPlugin from 'chartjs-plugin-annotation';
 import { Chart as ChartJS } from 'chart.js/auto'
 import { Bar } from 'react-chartjs-2'
@@ -14,7 +14,9 @@ const StatGraph = () => {
   const gameData = location.state?.gameData || []
   const selectedStat = location.state?.selectedStat
   const propLine = location.state?.propLine
-  const chartData = h2hData.length > 0 ? h2hData : gameData
+  const [minsPlayed, setMinsPlayed] = useState(68);
+  const rawChartData = h2hData.length > 0 ? h2hData : gameData;
+  const chartData = rawChartData.filter(game => game.min <= minsPlayed);
 
   return (
     <div className='text-xl flex flex-col md:flex-row items-center justify-center gap-4 h-screen bg-gradient-to-b from-blue-900 to-black text-white overflow-x-hidden px-4'>
@@ -118,7 +120,29 @@ const StatGraph = () => {
             },
           }}
         />
+        <div id='minute-slider' className='mt-[28px] -ml-[15px]'>
+          <input
+            type="range"
+            min="1"
+            max="68"
+            value={minsPlayed}
+            onChange={(e) => setMinsPlayed(e.target.value)}
+            className="h-3 w-[145px] cursor-pointer appearance-none rounded-xl bg-gray-600 border-1
+             [&::-webkit-slider-thumb]:appearance-none 
+             [&::-webkit-slider-thumb]:h-5 
+             [&::-webkit-slider-thumb]:w-5 
+             [&::-webkit-slider-thumb]:rounded-full 
+             [&::-webkit-slider-thumb]:bg-[url('https://png.pngtree.com/element_our/20190528/ourmid/pngtree-round-cartoon-basketball-image_1145574.jpg')] 
+             [&::-webkit-slider-thumb]:bg-contain 
+             [&::-webkit-slider-thumb]:bg-no-repeat 
+             [&::-webkit-slider-thumb]:bg-center 
+             [&::-webkit-slider-thumb]:border-none"
+          />
+          <p className="text-white text-sm -mt-[4px] ml-[5px]">Minutes played: &le; {minsPlayed}</p>
+        </div>
+
       </div>
+      
 
     </div>
   )
