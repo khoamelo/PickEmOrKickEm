761300b6c29d39b3838322467bed924a5c640eb3
Khoamelo <ckn2004@MSI.localdomain>
2025-07-01 11:03:26 -0700

    Updated connection pool to connect to DB on Neon

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 
 // Create a new connection pool using environment variables for configuration
-const pool = new Pool();
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+  : new Pool();
 
 // Export a query function that uses the pool to execute SQL queries
 // This allows other modules to run queries using: db.query(text, params)
